8535d01219ea18853deb0119c81dc6059340e398
Pranav Prakash <pranjp25@gmail.com>
2024-10-29 15:43:31 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({path: '.env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+//  FIRST APPROACH WHERE WE WRITE THE DATABASE CONNECTION CODE IN THE INDEX FILE ITSELF
+/*
+import express from "express"
+const app = express()
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",() =>{
+        console.log("ERROR:", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR:", error);
+    throw error;
+  }
+})();
+
+*/
