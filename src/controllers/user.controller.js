d467c15eef281ae89dcc53883ba23a05467a2f2d
Pranav Prakash <pranjp25@gmail.com>
2024-11-11 15:25:16 +0530

    debug regarding files uploading in cloudinary.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/Cloudinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
-  api_key: "process.env.CLOUDINARY_API_KEY",
-  api_secret: "process.env.CLOUDINART_API_SECRET",
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,10 +15,15 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been uploaded successfully
-    console.log("File is uploadedo on cloudinary", response.url);
+    console.log("File is uploaded on cloudinary", response.url);
+    //remove the locally saved temporary file
+    fs.unlinkSync(localFilePath);
     return response;
+    
+    
   } catch (error) {
-    fs.unlinkSync(localFilePath); //remove the locally saved temporary file
+    console.log("Error while uploading file on cloudinary", error);
+    
     // as the upload operation is failed.
   }
 };
